2e8fab54be80f64ab9b35c18161a4a46e78d65cc
Gabriel Souza <biel.als@icloud.com>
2023-02-28 21:49:38 -0300

    Lógica do registro do usuário implantada

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import { useNavigate } from 'react-router-dom'
+let users = []
 
 const schema = yup.object({ //validação 
     name: yup.string().required('O nome é obrigatório'),
@@ -14,8 +16,10 @@ const schema = yup.object({ //validação
   }).required();
 
 export default () => {
+    const navigate = useNavigate()
     const [state, setState ] = useState('')
     const [stateTwo, setStateTwo] = useState('')
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         window.matchMedia('(max-width: 700px)').matches &&  setState(window.matchMedia('(max-width: 700px)'))
@@ -25,10 +29,36 @@ export default () => {
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema) // passar como parametro quando se tem validação
     })
+
     const addUser = data => {
-        console.log(data)
+        getUsers(data.email)
+        if(users.length>0){
+            users.forEach(element => {
+                if(element === data.email) {
+                    setError(true)
+                }
+                else {
+                    localStorage.setItem(data.email, JSON.stringify(data))
+                    setError(false)
+                    navigate('/login')
+                }
+            })
+        }
+        else {
+            localStorage.setItem(data.email, JSON.stringify(data))
+            navigate('/login')
+        }
     } 
 
+    const getUsers = data => {
+        let userString = localStorage.getItem(`${data}`)
+        let userObj
+        if(userString!=null) {
+            userObj = JSON.parse(userString)
+            return users.push(userObj.email)
+        }
+        
+    }
 
     return (
         <form onSubmit={handleSubmit(addUser)}>
@@ -42,18 +72,19 @@ export default () => {
                 {!state.matches && (<label>Nome</label>) }
                 {!state.matches && (<InputText type='text' name='name' register={register} required='required: true' placeholder='Digite seu nome completo'/>) }
                 {!state.matches && (<label>Email</label>) }
-                {!state.matches && (<InputText type='email' name='email' register={register} required='required: true' placeholder='Escolha o seu melhor e-mail'/>) }
+                {!state.matches && (<InputText type='email' name='email' register={register} required='required: true' placeholder='Escolha o seu melhor e-mail' />) }
 
                 <label>Senha</label>
-                <InputText type='password' name='password' register={register} required='required: true' placeholder='Crie uma senha' classe={errors.passwordC ? 'border-red' : ''}/>
+                <InputText type='password' name='password' register={register} required='required: true' placeholder='Crie uma senha' classe={errors.passwordC ? 'border-red' : ''} />
                 <label>Confirme sua senha</label>
                 <InputText type='password' name='passwordC' register={register} required='required: true' placeholder='Repita a senha criada acima' classe={errors.passwordC ? 'border-red' : ''}/>
 
                 {errors.passwordC && (<p className='invalidPassword'>{errors.passwordC?.message}</p>)}
+                {error==true && (<p className='invalidEmail'>E-mail já cadastrado no sistema.</p>)}
 
                 {stateTwo.matches && (<Button class='register__button' text='Cadastrar!'/>)}
                 {!stateTwo.matches && (<Button class='register__button' text='Cadastrar'/>)}
-                
+
                 <Rodape />
             </div>
         </form>
